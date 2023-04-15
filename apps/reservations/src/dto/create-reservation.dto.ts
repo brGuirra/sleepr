a644b11b408ec29c3734dc50604829172a809545
brGuirra <br.guirra@gmail.com>
2023-04-15 16:46:17 -0300

    feat: add validation to reservations app

--- apps/reservations/src/dto/create-reservation.dto.ts
+++ apps/reservations/src/dto/create-reservation.dto.ts
@@ -1,9 +1,20 @@
+import { Type } from 'class-transformer';
+import { IsDate, IsNotEmpty, IsString } from 'class-validator';
+
 export class CreateReservationDto {
+  @IsDate()
+  @Type(() => Date)
   public startDate: Date;
 
+  @IsDate()
+  @Type(() => Date)
   public endDate: Date;
 
+  @IsNotEmpty()
+  @IsString()
   public placeId: string;
 
+  @IsNotEmpty()
+  @IsString()
   public invoiceId: string;
 }
