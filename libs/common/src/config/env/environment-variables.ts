f85b5e98856be66fc780e94670f2c37f126bb854
brGuirra <br.guirra@gmail.com>
2023-04-20 18:03:35 -0300

    chore: setup api with docker compose for development

--- libs/common/src/config/env/environment-variables.ts
+++ libs/common/src/config/env/environment-variables.ts
@@ -3,13 +3,5 @@ import { IsNotEmpty, IsString } from 'class-validator';
 export class EnvironmentVariables {
   @IsNotEmpty()
   @IsString()
-  public readonly MONGO_DB_USERNAME: string;
-
-  @IsNotEmpty()
-  @IsString()
-  public readonly MONGO_DB_PASSWORD: string;
-
-  @IsNotEmpty()
-  @IsString()
   public readonly MONGO_DB_URI: string;
 }
